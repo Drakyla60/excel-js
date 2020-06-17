c7ac3366e96b207f070ca21759b1f5d8841cb61c
Roma Volkov <Drakyla60@gmail.com>
2020-06-17 20:15:40 +0300

    Динамічний рендеринг компонентів

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,22 @@ export class Excel {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach(Component => {
+      const component = new Component()
+      // console.log(component.toHTML())
+      $root.insertAdjacentHTML('beforeend', component.toHTML())
+    })
+
+    return $root;
+  }
+
+  render() {
+    // console.log(this.$el);
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
+    this.$el.append(this.getRoot());
+  }
 }
