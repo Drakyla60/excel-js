0cd877cb9838aa62b67691c75b60678301e70b26
Roma Volkov <Drakyla60@gmail.com>
2020-06-21 20:44:23 +0300

    Генерування  таблиці

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,11 +3,11 @@ const CODES = {
   Z: 90
 }
 
-// function createCell() {
-//   return `
-//     <div class="cell" contenteditable></div>
-//   `
-// }
+function toCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
 
 function toColumn(column) {
   return `
@@ -15,10 +15,10 @@ function toColumn(column) {
   `
 }
 
-function createRow( content) {
+function createRow(index, content) {
   return `
   <div class="row">
-    <div class="row-info"></div>
+    <div class="row-info">${index ? index : ''}</div>
     <div class="row-data">${content}</div>
   </div>
   `
@@ -28,7 +28,7 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 20) {
+export function createTable(rowsCount = 10) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
@@ -38,10 +38,15 @@ export function createTable(rowsCount = 20) {
       .map(toColumn)
       .join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
-  for (let i = 0; i < colsCount; i++) {
-    rows.push(createRow())
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
+    rows.push(createRow(i + 1, cells))
   }
   return rows.join('')
 }
