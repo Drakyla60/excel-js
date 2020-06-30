a6b7f3e4c2251ab85203272273d9e902ccd7a155
Roma Volkov <Drakyla60@gmail.com>
2020-06-30 21:32:27 +0300

    fix ESlint ті пропуски глючат

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
 import {resizeHandler} from '@/components/table/table.resize';
-import {isSell, matrix, nextSelector, shouldResize} from
-    '@/components/table/table.functions';
+import {isSell, matrix, nextSelector, shouldResize}
+  from '@/components/table/table.functions';
 import {TableSelection} from '@/components/table/TableSelection';
 import {$} from '@core/dom';
 
