69efc456096d193e637d60806ff2038cfc195b1f
Roma Volkov <Drakyla60@gmail.com>
2020-07-05 17:07:24 +0300

    Створення інтерактиву Формули і клітинок таблиці

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -12,7 +12,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options
     });
   }
@@ -28,12 +28,20 @@ export class Table extends ExcelComponent {
   init() {
     super.init();
 
-    const $cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select($cell)
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
 
     this.$on('formula:input', text => {
       this.selection.current.text(text)
     })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
   }
 
   onMousedown(event) {
@@ -42,8 +50,8 @@ export class Table extends ExcelComponent {
     } else if (isSell(event)) {
       const $target = $(event.target)
       if (event.shiftKey) {
-        const $cells = matrix($target, this.selection.current)
-            .map(id => this.$root.find(`[data-id="${id}"]`))
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
         this.selection.select($target)
@@ -67,9 +75,12 @@ export class Table extends ExcelComponent {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
-      console.log(key)
+      this.selectCell($next)
     }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
 
