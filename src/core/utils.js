17e989acd346a9dd97a6911cd4d937f632cd0f90
Roma Volkov <Drakyla60@gmail.com>
2020-07-07 21:33:08 +0300

    Глюки з тими відступами

--- src/core/utils.js
+++ src/core/utils.js
@@ -10,9 +10,7 @@ export function range(start, end) {
     [end, start] = [start, end]
   }
 
-  return new Array(end - start + 1)
-    .fill('')
-    .map((_, index) => start + index)
+  return new Array(end - start + 1).fill('').map((_, index) => start + index)
 }
 
 export function storage(key, data = null) {
