9cf128fa1f0e25de1c9a1ea4db0afca5c2253ea0
Roma Volkov <Drakyla60@gmail.com>
2020-07-13 00:18:23 +0300

    Тестування Router.js

--- babel.config.js
+++ babel.config.js
@@ -9,5 +9,8 @@ module.exports = {
       }
     ],
   ],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    // '@babel/plugin-transform-modules-commonjs',
+  ]
 }
